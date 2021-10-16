1ad9ac5a2ffb5a1e1a41fdbc8b11dc9dc36424d0
Abhishek Jain <ajj.2396@gmail.com>
2021-10-16 18:52:15 +0530

    Fix Invariant Violation error

--- App.js
+++ App.js
@@ -1,16 +1,30 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-// import { NativeBaseProvider, Box } from "native-base";
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  View,
+} from "react-native";
+import { NativeBaseProvider, Box } from "native-base";
 
 export default function App() {
   return (
-    // <NativeBaseProvider>
+    <NativeBaseProvider>
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
+        <Text>Counter: 0</Text>
+        <View style={styles.floatingView}>
+          <TouchableOpacity style={styles.floatingButton}>
+            <Text>+</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.floatingButton}>
+            <Text>-</Text>
+          </TouchableOpacity>
+        </View>
+        <TextInput keyboardType="numeric" placeholder="change amount" />
       </View>
-    // </NativeBaseProvider>
+    </NativeBaseProvider>
   );
 }
 
