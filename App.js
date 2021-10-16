c402a79ceaf7a0993bb532d8420f4919a45119a4
Abhishek Jain <ajj.2396@gmail.com>
2021-10-16 19:54:05 +0530

    Use redux-persist

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { NativeBaseProvider} from "native-base";
+import { NativeBaseProvider } from "native-base";
 import { Provider } from "react-redux";
-import store from "./store";
+import { store, persistor } from "./store";
+import { PersistGate } from "redux-persist/integration/react";
 
 import SimpleCounter from "./SimpleCounter";
 
 export default function App() {
   return (
     <Provider store={store}>
-      <NativeBaseProvider>
-        <SimpleCounter />
-      </NativeBaseProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <NativeBaseProvider>
+          <SimpleCounter />
+        </NativeBaseProvider>
+      </PersistGate>
     </Provider>
   );
 }
