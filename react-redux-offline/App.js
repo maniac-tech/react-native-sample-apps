aac1faf6eb10aa93dea5e77b4be680a324a06001
Abhishek Jain <ajj.2396@gmail.com>
2021-10-18 01:14:13 +0530

    Base code to check whether internet is available or not

--- react-redux-offline/App.js
+++ react-redux-offline/App.js
@@ -2,18 +2,28 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { NativeBaseProvider } from "native-base";
 import { Provider } from "react-redux";
+import { NetworkProvider, useIsConnected } from "react-native-offline";
 import store from "./store";
 
-import ConnectivityStatus from './ConnectivityStatus';
+import ConnectivityStatus from "./ConnectivityStatus";
 import SimpleCounter from "./SimpleCounter";
 
 export default function App() {
+  const serverURLForPing = "https://google.com";
+
   return (
     <Provider store={store}>
-      <NativeBaseProvider>
-        <ConnectivityStatus />
-        <SimpleCounter />
-      </NativeBaseProvider>
+      <NetworkProvider
+        pingInterval={30000}
+        pingOnlyIfOffline={true}
+        pingInBackground={true}
+        pingServerUrl={serverURLForPing}
+      >
+        <NativeBaseProvider>
+          <ConnectivityStatus />
+          <SimpleCounter />
+        </NativeBaseProvider>
+      </NetworkProvider>
     </Provider>
   );
 }
--- react-redux-offline/ConnectivityStatus.jsx
+++ react-redux-offline/ConnectivityStatus.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { useIsConnected } from "react-native-offline";
 
-const ConnectivityStatus = () => {
-  const connectionStatus = "available";
+const ConnectivityStatus = (props) => {
+  const isInternetAvailable = useIsConnected();
+  console.log("isInternetAvailable:", isInternetAvailable);
+
   return (
     <View style={styles.container}>
-      <Text>Internet is: {connectionStatus}</Text>
+      <Text>
+        Internet is: {isInternetAvailable ? "available" : "not available"}
+      </Text>
     </View>
   );
 };
